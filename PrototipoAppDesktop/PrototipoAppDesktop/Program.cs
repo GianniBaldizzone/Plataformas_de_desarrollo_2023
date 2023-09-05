6c442affdc03c5b5e7d0643e1a9a6ccec279fb64
Gianni Baldizzone <gianni.baldizzone@davinci.edu.ar>
2023-09-05 11:02:12 -0300

    Conexion con la base de datos creada

--- PrototipoAppDesktop/PrototipoAppDesktop/Program.cs
+++ PrototipoAppDesktop/PrototipoAppDesktop/Program.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -16,6 +18,27 @@ namespace PrototipoAppDesktop
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+
+            var builder = SqlConnectionStringBuilder();
+            builder.DataSource = "DESKTOP-CGPE47L\\MSSQLSERVER01";// NOMBRE DEL SERVIDOR;
+            builder.InitialCatalog = "prueba";//NOMBRE DE LA BD
+            builder.IntegratedSecurity = true;
+
+            string conectionString = builder.ToString();
+            SqlConnection concection = new SqlConnection(conectionString);
+            Trace.WriteLine("Conexión a la BD: " + concection);
+
+            try
+            {
+                concection.Open();
+                concection.Close();
+                Trace.WriteLine("Conexion creada con exito");
+            }
+            catch (Exception e)
+            {
+                Trace("Hubo algun error en al conexión" + e.Message);
+            }
             Application.Run(new Form1());
         }
     }
