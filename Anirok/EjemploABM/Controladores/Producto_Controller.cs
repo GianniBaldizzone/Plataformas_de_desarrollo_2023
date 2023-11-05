15c9d7ed976f4c0c99dc8bca2a35cede3a55b69f
Gianni Baldizzone <gianni.baldizzone@davinci.edu.ar>
2023-11-05 19:43:46 -0300

    Clase Producto y ProductoController creados

---
+++ Anirok/EjemploABM/Controladores/Producto_Controller.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EjemploABM.Controladores
+{
+    internal class Class1
+    {
+    }
+}
